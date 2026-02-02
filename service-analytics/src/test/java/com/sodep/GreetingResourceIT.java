3e2af917c258f195e193754655fe050fe0877050
josepe <josepedrozo99@gmail.com>
2026-02-02 09:46:39 -0300

    first commit

---
+++ service-analytics/src/test/java/com/sodep/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.sodep;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
